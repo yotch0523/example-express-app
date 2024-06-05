116af0a55cdc490f197dc0d0f0cc6c7f7919244e
Youki YOSHIOKA <yotch.youki.you@gmail.com>
2024-06-05 18:02:25 +0900

    fixed index.ts and git ignore

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,10 @@ const app = express();
 
 // create api endpoint returns "Hello, world" with GET METHOD
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world');
+  res.send('Hello, world. This api is on Azure Web App Service.');
 });
 
 // setup server to listen on port 3000
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
